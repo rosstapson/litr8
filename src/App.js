bb9408bc20d41b74f32aab38814d445826b27439
rosstapson <ross.tapson@gmail.com>
2018-08-01 09:12:24 +0200

    some new components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 import MenuAppBar from './MenuAppBar'
 import NewPostWidget from './components/NewPostWidget';
-
+// import RecipeReviewCard from './components/RecipeReviewCard';
+// <RecipeReviewCard />
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MenuAppBar />
         <NewPostWidget />
+        
       </div>
     );
   }
